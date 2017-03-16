b5a9a2befa19a49d669042784156949749215d7f
mike <mike@exophase.com>
2017-03-16 14:43:33 -0400

    Footer

--- wp-content/themes/cfe-redesign/templates/footer.php
+++ wp-content/themes/cfe-redesign/templates/footer.php
@@ -1,8 +1,74 @@
+<?php if(!is_home()) { ?>
 <footer class="content-info">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+
+	<div class="row">
+
+		<div class="col-sm-12 col-md-3 col-lg-3 static">
+			<div class="box logo-container">
+				<div class="inner">
+					<img class="logo img-responsive" src="/wp-content/themes/cfe-redesign/dist/images/signature-stationery_2lines_white.png" alt="Center for Entrepreneurship">
+
+					<div class="address">
+						<a target="_blank" href="https://www.google.com/maps/dir/''/2281+Bonisteel+Boulevard+%7C+3350+Duderstadt+Center+Ann+Arbor+MI,+48109/@42.291142,-83.7857578,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x883cae86199a08ed:0x80181def6c73cb99!2m2!1d-83.7157179!2d42.2911633">
+							<span>2281 Bonisteel Boulevard</span>
+							<span>3350 Duderstadt Center</span>
+							<span>Ann Arbor MI, 48109</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-sm-5 col-md-4 col-lg-3 sidebars col-xs-12">
+			<div class="inner">
+				<h3>Contact Us</h3>
+				<div class="textwidget">
+				<p><span>P:</span><a href="tel:7347631021">734.763.1021</a></p>
+				<p><span>F:</span><a href="tel:7346157028">734.615.7028</a></p>
+				<p><span>E:</span><a href="mailto:centerforentrepreneurship@umich.edu">centerforentrepreneurship@umich.edu</a></p>
+				</div>
+			</div>
+		</div>
+
+		<div class="box col-sm-3 col-md-2 col-lg-2 col-xs-12">
+			<div class="inner">
+				<h3>Social</h3>
+				<div class="textwidget">
+					<ul class="social-footer">
+						<li>
+							<a href="https://www.facebook.com/UMCFE" target="_blank"><i class="fa fa-facebook"></i></a>
+						</li>
+						<li>
+							<a href="https://twitter.com/UMCFE" target="_blank"><i class="fa fa-twitter"></i></a>
+						</li>
+						<li>
+							<a href="https://www.instagram.com/umcfe/" target="_blank"><i class="fa fa-instagram"></i></a>
+						</li>
+						<li>
+							<a href="https://www.linkedin.com/company-beta/2883825/" target="_blank"><i class="fa fa-linkedin"></i></a>
+						</li>
+						<li>
+							<a href="https://www.youtube.com/channel/UCJ8GjIBM1m_6PYRuCSETz-g" target="_blank"><i class="fa fa-youtube"></i></a>
+						</li>
+					</ol> 
+				</div>
+			</div>
+		</div>
+
+		<div class="col-sm-4 col-md-3 col-lg-4 footer-buttons col-xs-12">
+			<div class="inner">
+				<a target="_blank" href="https://leadersandbest.umich.edu/find/#!/give/basket/fund/313016">Give Now</a>
+				<a target="_blank" href="https://goo.gl/forms/dXrTCqDpoFDCTLGC3">Join Our Newsletter</a>
+			</div>
+		</div>
+
+	</div>
+
+
   </div>
 </footer>
+<?php } ?>
 
 <div class="navigation-bottom">
 	<?php get_template_part('templates/search', 'block'); ?>
