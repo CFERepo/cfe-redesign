2658df4efd85c3687a51bf8ac9b6556ec6bd7ca2
mike <mike@exophase.com>
2017-03-20 12:44:09 -0400

    image sizes

--- wp-content/themes/cfe-redesign/lib/setup.php
+++ wp-content/themes/cfe-redesign/lib/setup.php
@@ -125,7 +125,7 @@ function display_sidebar() {
  * Theme assets
  */
 function assets() {
-  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css?4'), false, null);
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
